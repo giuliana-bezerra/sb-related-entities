ea58f268aaa1ebd06d9b6b08a64e52f95c8a06bb
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2023-03-20 17:07:28 -0300

    add Oracle solution using keyHolder

--- src/main/java/br/com/giulianabezerra/sbrelatedentities/WriterConfig.java
+++ src/main/java/br/com/giulianabezerra/sbrelatedentities/WriterConfig.java
@@ -7,24 +7,29 @@ import javax.sql.DataSource;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 @Configuration
 public class WriterConfig {
-
   @Bean
   ItemWriter<Venda> writer(DataSource dataSource) {
     return chunk -> {
-      SimpleJdbcInsert jdbcVendas = new SimpleJdbcInsert(dataSource)
-          .withTableName("vendas")
-          .usingGeneratedKeyColumns("id");
+      List<? extends Venda> vendas = chunk.getItems();
+      SqlParameterSource[] batch = SqlParameterSourceUtils.createBatch(vendas.toArray());
+      KeyHolder keyHolder = new GeneratedKeyHolder();
+      NamedParameterJdbcTemplate jdbcVendas = new NamedParameterJdbcTemplate(dataSource);
 
-      chunk.getItems().forEach(venda -> {
-        BeanPropertySqlParameterSource vendaParams = new BeanPropertySqlParameterSource(venda);
-        Long idVenda = jdbcVendas.executeAndReturnKey(vendaParams).longValue();
+      for (int i = 0; i < batch.length; i++) {
+        jdbcVendas.update("INSERT INTO vendas (description) VALUES (:description)", batch[0], keyHolder,
+            new String[] { "id" });
+        Long idVenda = keyHolder.getKey().longValue();
 
+        Venda venda = chunk.getItems().get(i);
         List<ItemVenda> itensVenda = venda.itensVendas().stream()
             .map(itemVenda -> new ItemVenda(itemVenda.id(), itemVenda.description(), idVenda))
             .toList();
@@ -32,7 +37,7 @@ public class WriterConfig {
         SimpleJdbcInsert jdbcItensVendas = new SimpleJdbcInsert(dataSource)
             .withTableName("itens_vendas");
         jdbcItensVendas.executeBatch(SqlParameterSourceUtils.createBatch(itensVenda));
-      });
+      }
     };
   }
 }
